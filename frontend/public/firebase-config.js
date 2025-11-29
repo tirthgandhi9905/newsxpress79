e5ce2afdb3ea34890bd2ec8be513132d1032929f
Tirth Nirav Gandhi <162357228+tirthgandhi9905@users.noreply.github.com>
2025-11-29 23:30:01 +0530

    Add Firebase configuration file

---
+++ frontend/public/firebase-config.js
@@ -0,0 +1,9 @@
+self.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC7QCNQMNJ5ozKFV7HkRdyQ3MzUuZJMtYc",
+  authDomain: "newsxpress-248ef.firebaseapp.com",
+  projectId: "newsxpress-248ef",
+  storageBucket: "newsxpress-248ef.firebasestorage.app",
+  messagingSenderId: "1035708581862",
+  appId: "1:1035708581862:web:7313e17226571081d57827",
+  measurementId: "G-GCZXCF0LPL",
+};
